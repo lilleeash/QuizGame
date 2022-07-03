0bd227431b53adc94def06ecab092447fb9af0bf
Darya Zhitova <daryazhitova@Daryas-MacBook-Air.local>
2022-07-03 11:49:59 +0300

    Start project files

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Darya Zhitova on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
